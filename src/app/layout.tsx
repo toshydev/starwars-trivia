d069102bca6602622cf7bd969fb9c7f637ee0cd8
Anton Roters <antonroters@hotmail.com>
2023-10-09 14:19:39 +0200

    add favicon(not working)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Star Wars Trivia",
   description: "Information about Star Wars films, characters, and more.",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
